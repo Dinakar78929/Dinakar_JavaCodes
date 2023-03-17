2ae2726730e63c9d43b4ecb447e677eb7d7ce29b
Dinakar <dinakar.m.xworkz@gmail.com>
2023-03-17 14:15:35 +0530

    March17 test

---
+++ head-phone/src/com/xworkz/headphone/repo/HeadPhoneRepository.java
@@ -0,0 +1,9 @@
+package com.xworkz.headphone.repo;
+
+import com.xworkz.headphone.dto.HeadPhoneDTO;
+
+public interface HeadPhoneRepository {
+	
+	boolean save(HeadPhoneDTO dto);
+
+}
---
+++ head-phone/src/com/xworkz/headphone/repo/HeadPhoneRepositoryImpl.java
@@ -0,0 +1,16 @@
+package com.xworkz.headphone.repo;
+
+import com.xworkz.headphone.dto.HeadPhoneDTO;
+
+public class HeadPhoneRepositoryImpl implements HeadPhoneRepository{
+	
+	private HeadPhoneDTO[] headPhones=new HeadPhoneDTO[5];
+	private int headphoneIndex[0]
+
+	@Override
+	public boolean save(HeadPhoneDTO dto) {
+		
+		return false;
+	}
+
+}
