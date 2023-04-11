4f08f4deaf22e1ce608f2ae8100e645948108279
Dinakar <dinakar.m.xworkz@gmail.com>
2023-04-11 10:19:04 +0530

    Collections

--- collections/src/com/xworkz/collections/SambarPowder.java
+++ collections/src/com/xworkz/collections/SambarPowder.java
@@ -1,7 +1,9 @@
 package com.xworkz.collections;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class SambarPowder {
 
@@ -10,8 +12,39 @@ public class SambarPowder {
 		powders.add("MTR");
 		powders.add("RGB");
 		powders.add("Iyengar");
-		powders.add("Archi");
+		powders.add("Achi");
 		powders.add(null);
+		powders.add("Shivam");
+		powders.add("MDH");
+		powders.add("Catch");
+		
+		System.out.println(powders);
+		System.out.println("Total size:"+powders.size());
+		
+		for(String powder:powders)
+		{
+			if(Objects.nonNull(powders))
+			{
+				System.out.println(powder);
+			}
+		}
+		System.out.println(powders.size());
+		
+		Iterator<String> itr=powders.iterator();
+		System.out.println("--------After iterator-------");
+		while(itr.hasNext())
+		{
+			if(Objects.nonNull(itr))
+			{
+				String ref=itr.next();
+				if(Objects.isNull(ref))
+				{
+					itr.remove();
+				}
+			}
+		}
+		System.out.println(powders);
+		System.out.println("Total size:"+powders.size());
 
 	}
 
