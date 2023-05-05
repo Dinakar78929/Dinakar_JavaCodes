f28244531bcc00f262684208c83ba0ab7c25128b
Dinakar <dinakar.m.xworkz@gmail.com>
2023-05-05 10:24:27 +0530

    crud operation using spring

---
+++ spring-crud-operation/src/com/xworkz/spring/springConfigure/SockConfigure.java
@@ -0,0 +1,26 @@
+package com.xworkz.spring.springConfigure;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+@Configuration
+@Component("com.xworkz.spring")
+public class SockConfigure {
+	public SockConfigure() {
+		System.out.println("no args sockConfigure");
+	}
+	
+	@Bean
+	public Validator validator() {
+		System.out.println("Registered validator ");
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		return validator;
+
+	}
+
+}
